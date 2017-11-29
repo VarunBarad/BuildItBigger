4929f3d85a9f4833e54bfadbcfdcb418732e9b38
Varun Barad <barad.varun29@gmail.com>
2017-11-29 18:49:27 +0530

    Add progress-dialog in the activity while the joke loads.

--- app/src/main/java/com/varunbarad/builditbigger/app/MainActivity.java
+++ app/src/main/java/com/varunbarad/builditbigger/app/MainActivity.java
@@ -1,5 +1,6 @@
 package com.varunbarad.builditbigger.app;
 
+import android.app.ProgressDialog;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -11,6 +12,7 @@ import com.varunbarad.builditbigger.jokesdisplaylibrary.DisplayJokeActivity;
 
 
 public class MainActivity extends AppCompatActivity {
+  private ProgressDialog progressDialog;
   
   private ActivityMainBinding dataBinding;
   
@@ -26,10 +28,30 @@ public class MainActivity extends AppCompatActivity {
     FetchJokeTask fetchJokeTask = new FetchJokeTask(new JokeListener() {
       @Override
       public void jokeReceived(Joke joke) {
+        MainActivity.this.dismissProgressDialog();
         DisplayJokeActivity.start(MainActivity.this, joke);
       }
     });
   
     fetchJokeTask.execute();
+    this.showProgressDialog();
+  }
+  
+  private void showProgressDialog() {
+    this.dismissProgressDialog();
+    
+    this.progressDialog = new ProgressDialog(this);
+    this.progressDialog.setCancelable(false);
+    this.progressDialog.setIndeterminate(true);
+    this.progressDialog.setTitle("Fetching joke");
+    this.progressDialog.setMessage("Please wait a while...");
+    this.progressDialog.show();
+  }
+  
+  private void dismissProgressDialog() {
+    if (this.progressDialog != null && this.progressDialog.isShowing()) {
+      this.progressDialog.dismiss();
+      this.progressDialog = null;
+    }
   }
 }
